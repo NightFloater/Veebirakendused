e78c1a371c4f42f3241a22d4f73f47b78cdb5725
NightFloater <kukepea2@gmail.com>
2018-04-29 22:26:48 +0300

    Deploy

--- src/main/webapp/upload.php
+++ src/main/webapp/upload.php
@@ -17,20 +17,28 @@
 
 
 
-<p>Kliki siia, et oma reso naha</p>
+<p>Kliki siia, et oma reso naha ja mis kellaajal kylastasid</p>
 
 <button onclick="myFunction()">Try it</button>
 
+
+
 <p id="X"></p>
 <p id="Y"></p>
+<p id="today"></p>
+
+
 
 <script>
     function myFunction() {
         var x = "Total Width: " + screen.width + "px";
-        var y = "Total Width: " + screen.width + "px";
+        var y = "Total Width: " + screen.height + "px";
 
         document.getElementById("X").innerHTML = x;
         document.getElementById("Y").innerHTML = y;
+
+        var today = new Date();
+        document.getElementById('time').innerHTML=today.getHours().toString();
     }
 </script>
 
