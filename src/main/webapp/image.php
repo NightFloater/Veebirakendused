b77a0595d89f787d88a7d3f75f2f44905168827d
NightFloater <kukepea2@gmail.com>
2018-04-29 22:15:26 +0300

    Deploy

--- src/main/webapp/image.php
+++ src/main/webapp/image.php
@@ -1,30 +1,19 @@
+<!DOCTYPE html>
 <html>
+<body>
 
-<form action="upload.php" method="POST">
-    <input type="hidden" name="width" id="width" value="">
-    <input type="hidden" name="height" id="height" value="">
-    <input type="submit" value="Resolution" name="submit">
-</form>
-<script type="text/javascript">
-    var  findwidth = window.width;
-    var  findheight =window.height;
+<p>Click the button to display the total width of your screen, in pixels.</p>
 
-    document.getElementById("width").value=findwidth;
-    document.getElementById("height").value=findheight;
+<button onclick="myFunction()">Try it</button>
 
+<p id="demo"></p>
+
+<script>
+    function myFunction() {
+        var x = "Total Width: " + screen.width + "px";
+        document.getElementById("demo").innerHTML = x;
+    }
 </script>
+
+</body>
 </html>
-
-
-<?php
-if(isset($_POST['submit']))
-{
-    $width = $_POST['width'];
-    $height = $_POST['height'];
-    echo "<p>$height,$width";
-
-}
-
-
-
-?>
