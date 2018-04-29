545541d0e9f44cb4f5ce1363f64a8ac3ea251a66
NightFloater <kukepea2@gmail.com>
2018-04-29 22:08:39 +0300

    Deploy

--- src/main/webapp/image.php
+++ src/main/webapp/image.php
@@ -1,28 +1,28 @@
+<form action="upload.php" method="POST">
+    <input type="hidden" name="width" id="width" value="">
+    <input type="hidden" name="height" id="height" value="">
+    <input type="submit" value="Resolution" name="submit">
+</form>
+<script type="text/javascript">
+    var  findwidth = window.width;
+    var  findheight =window.height;
+
+    document.getElementById("width").value=findwidth;
+    document.getElementById("height").value=findheight;
+
+</script>
+
+
+
 <?php
-$uploaddir = '/home/postgres/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-$name = $_POST['name'];
-
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
-{   // echo "File is valid, and was successfully uploaded.\n";
-}
-else   {   echo "File size greater than 300kb!\n\n";   }
-
-echo "'$name'\n";
-
-$conn = pg_pconnect("dbname=jdbc:postgresql://ec2-54-243-213-188.compute-1.amazonaws.com:5432/deoqpobdfumna2 user=vkzivsefpcoxqi password=dc800fc78ba20df40f86c5c828c8a4b69dce75095371428e732ca89f2c36b080");
-$query = "insert into image values ('$name', lo_import('$uploadfile'), 'now')";
-$result = pg_query($query);
-
-if($result)
+if(isset($_POST['submit']))
 {
-    echo "File is valid, and was successfully uploaded.\n";
-    unlink($uploadfile);
+    $width = $_POST['width'];
+    $height = $_POST['height'];
+    echo "$height,$width";
+
 }
-else
-{
-    echo "Filename already exists. Use another filename. Enter all the values.";
-    unlink($uploadfile);
-}
-pg_close($conn);
+
+
+
 ?>
