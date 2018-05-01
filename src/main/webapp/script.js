e8a80c8b2981e50e292fee917ceb32df05788e1d
NightFloater <kukepea2@gmail.com>
2018-05-01 17:25:28 +0300

    Deploy

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,5 +1,5 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#pic").attr('src',"http://i1.kym-cdn.com/photos/images/newsfeed/000/270/676/83b.png");
-  .
+
 }
